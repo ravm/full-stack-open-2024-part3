a77b4f68eac217c9a30498bd3e489bec0c8572bf
ravm <miikaravi99@gmail.com>
2024-02-15 12:27:57 +0200

    3.2 done

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const req_date = new Date()
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${req_date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${3001}`)
