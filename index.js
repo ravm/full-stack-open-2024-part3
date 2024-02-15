3c2eb2eaf81944af6a09b3c14b800c7213ffa88d
ravm <miikaravi99@gmail.com>
2024-02-15 12:57:37 +0200

    3.5 done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -53,6 +55,13 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id !== id)
 
     res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const person = req.body
+    person.id = Math.floor(Math.random() * 1000)
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 const PORT = 3001
