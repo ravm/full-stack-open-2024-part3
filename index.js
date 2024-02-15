6180b5917ee5c764d49015626ed25690acac44bd
ravm <miikaravi99@gmail.com>
2024-02-15 13:16:27 +0200

    3.6 done

--- index.js
+++ index.js
@@ -58,8 +58,24 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 app.post('/api/persons', (req, res) => {
-    const person = req.body
-    person.id = Math.floor(Math.random() * 1000)
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number missing'
+        })
+    } else if (persons.some((p) => p.name === body.name)) {
+        return res.status(409).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: Math.floor(Math.random() * 1000),
+        name: body.name,
+        number: body.number
+    }
+
     persons = persons.concat(person)
     res.json(person)
 })
