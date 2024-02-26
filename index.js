fef9904d350aab7cc1a45e7068deb416ab4c26ff
ravm <miikaravi99@gmail.com>
2024-02-26 09:14:22 +0200

    3.9 done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 let persons = [
@@ -91,7 +93,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${3001}`)
 })
